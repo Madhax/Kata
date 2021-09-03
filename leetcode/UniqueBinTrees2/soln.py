b3bab4200aadf3471f6348e77d786187b79f7f43
Patrick Gryciuk <chinchilla@gmail.com>
2021-09-02 22:48:16 -0400

    leetcode daily

---
+++ leetcode/UniqueBinTrees2/soln.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        
+        
+        def helper(cand):
+            
+            if len(cand) == 0:
+                return [None]
+            
+            
+            output = []
+            for root in cand:
+                leftTree = helper([x for x in cand if x < root])
+                rightTree = helper([x for x in cand if x > root])
+                
+                t = TreeNode(root)
+                #output += [TreeNode(val=root, left=l, right=r) for l in leftTree for r in rightTree]
+                
+                
+                for l in leftTree:
+                    for r in rightTree:
+                        t = TreeNode(root, l, r)
+                        #t.left = l
+                        #t.right = r
+                        output.append(t)
+                
+            return output
+            
+        
+        
+        #l = [1,2,3]
+        #r = [10, 11, 12]
+        
+        #print([[x, y] for x in l for y in r])
+        return helper([x for x in range(1, n+1)])
